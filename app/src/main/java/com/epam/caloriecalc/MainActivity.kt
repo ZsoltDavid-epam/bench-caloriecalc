d6b1082c0fe23cf966334d7ead3ed72adf215b72
patrik1katai <patrik1katai@gmail.com>
2022-01-18 14:21:42 +0100

    BENCH-7 - Hilt dependency injection tool implemented

--- app/src/main/java/com/epam/caloriecalc/MainActivity.kt
+++ app/src/main/java/com/epam/caloriecalc/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.epam.caloriecalc.ui.home.HomeScreen
 import com.epam.caloriecalc.ui.theme.CalorieCalcTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +20,9 @@ class MainActivity : ComponentActivity() {
             CalorieCalcTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalorieCalcTheme {
-        Greeting("Android")
     }
 }
