33d1042aa70444174519babcf03ef5551dfa7b6b
Zsolt_David <zsolt_david@epam.com>
2022-01-19 10:23:01 +0100

    Detekt initial config
    - Added detekt dependency
    - Changed base configuration to be compatible with Compose
    - Fixed base warnings (except for unused ones)

--- app/src/main/java/com/epam/caloriecalc/MainActivity.kt
+++ app/src/main/java/com/epam/caloriecalc/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.epam.caloriecalc.ui.home.HomeScreen
 import com.epam.caloriecalc.ui.theme.CalorieCalcTheme
 import dagger.hilt.android.AndroidEntryPoint
